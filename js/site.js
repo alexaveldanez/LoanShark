3ae718a74b246557dd4d686988b8d7b7bd71a4a5
Alex Aveldanez <alexsaveldanez@gmail.com>
2022-01-08 09:48:09 -0800

    Added landing page
    Added landing page logo and fixed the alignment.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,22 @@
 
-function HelloWorld() {
-    alert("Hello World");
+// get values from inputs on the UI
+function getValues() {
+// reset the DOM
+
+// get values from the 3 inputs stored into variables
+
+// put values into calculate loan function and run function
+
+// get results from calculate loan function into displayData function
+
+}
+
+// with the values selected calculate loan
+function calculateLoan() {
+
+}
+
+// display data onto the UI
+function displayData() {
+
 }
